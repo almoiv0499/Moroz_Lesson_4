9e22811181d3b1c777f5661b393bc73bc66b447b
Aleksandr Moroz <morozaleksandriv@gmail.com>
2023-02-19 14:16:54 +0300

    feature_clock created AnalogClockView.kt

---
+++ app/src/main/java/com/example/moroz_lesson_4/AnalogClockView.kt
@@ -0,0 +1,15 @@
+package com.example.moroz_lesson_4
+
+import android.content.Context
+import android.icu.util.Calendar
+import android.util.AttributeSet
+import android.view.View
+
+
+class AnalogClockView(
+    context: Context,
+    attrs: AttributeSet?,
+    defResStyle: Int
+) : View(context, attrs, defResStyle) {
+
+}
