4193fc5f2d51b0ead41d922107b2aa60c7cf1487
Aleksandr Moroz <morozaleksandriv@gmail.com>
2023-02-19 18:24:51 +0300

    feature_clock set attributes via code and via xml

--- app/src/main/java/com/example/moroz_lesson_4/MainActivity.kt
+++ app/src/main/java/com/example/moroz_lesson_4/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.moroz_lesson_4
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.moroz_lesson_4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setColorForClockHands()
     }
+
+    private fun setColorForClockHands() {
+        with(binding) {
+            analogClock.setHourHandColor(
+                ContextCompat.getColor(
+                    this@MainActivity, R.color.hour_hand_color
+                )
+            )
+            analogClock.setMinuteHandColor(
+                ContextCompat.getColor(
+                    this@MainActivity, R.color.minute_hand_color
+                )
+            )
+            analogClock.setSecondHandColor(
+                ContextCompat.getColor(
+                    this@MainActivity, R.color.second_hand_color
+                )
+            )
+        }
+    }
+
 }
