bc7396adf3eeaf02ad80a5f4f51496b3636360ac
Aleksandr Moroz <morozaleksandriv@gmail.com>
2023-02-19 15:12:55 +0300

    feature_clock drew clock dial

--- app/src/main/java/com/example/moroz_lesson_4/AnalogClockView.kt
+++ app/src/main/java/com/example/moroz_lesson_4/AnalogClockView.kt
@@ -1,10 +1,12 @@
 package com.example.moroz_lesson_4
 
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Color
-import android.icu.util.Calendar
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import kotlin.math.min
 
 
 class AnalogClockView @JvmOverloads constructor(
@@ -13,31 +15,92 @@ class AnalogClockView @JvmOverloads constructor(
     defResAttrs: Int = 0,
 ) : View(context, attrs, defResAttrs) {
 
+    companion object {
+        // Size const
+        private const val DEFAULT_SIZE_VALUE = 0
+        private const val PADDING = 50
+        private const val DIVIDER_BY_HALF = 2
 
-    init {
-        setCustomAttributes(attrs = attrs)
+        // Stroke width const
+        private const val DEFAULT_STROKE_WIDTH = 0.0F
+        private const val CIRCLE_STROKE_WIDTH = 10F
     }
 
-    private fun setCustomAttributes(attrs: AttributeSet?) {
+    // Paint circle
+    private var circlePaint: Paint? = null
+    private var mWidth = DEFAULT_SIZE_VALUE
+    private var mHeight = DEFAULT_SIZE_VALUE
+    private var centreX = DEFAULT_SIZE_VALUE
+    private var centreY = DEFAULT_SIZE_VALUE
+    private var padding = DEFAULT_SIZE_VALUE
+    private var radius = DEFAULT_SIZE_VALUE
+    private var minimum = DEFAULT_SIZE_VALUE
+
+    init {
+        initAllPaints()
+        setCustomAttrs(attrs = attrs)
+    }
+
+    private fun initParams() {
+        mWidth = width
+        mHeight = height
+        padding = PADDING
+
+        centreX = mWidth / DIVIDER_BY_HALF
+        centreY = mHeight / DIVIDER_BY_HALF
+        minimum = min(mHeight, mWidth)
+        radius = minimum / DIVIDER_BY_HALF - padding
+
+    }
+
+    private fun setCustomAttrs(attrs: AttributeSet?) {
         if (attrs == null) return
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.AnalogClockView)
 
         with(typedArray) {
             // Color for clock hands
-            val colorHourHand = getColor(R.styleable.AnalogClockView_colorHourHand, Color.RED)
-            val colorMinuteHand = getColor(R.styleable.AnalogClockView_colorMinuteHand, Color.BLUE)
-            val colorSecondHand = getColor(R.styleable.AnalogClockView_colorSecondHand, Color.GRAY)
+            val hourHandColor = getColor(R.styleable.AnalogClockView_colorHourHand, Color.RED)
+            val minuteHandColor = getColor(R.styleable.AnalogClockView_colorMinuteHand, Color.BLUE)
+            val secondHandColor = getColor(R.styleable.AnalogClockView_colorSecondHand, Color.GRAY)
 
             // Stroke width for clock hands
-            val strokeWidthHourHand =
-                getFloat(R.styleable.AnalogClockView_strokeWidthHourHand, 0.0F)
-            val strokeWidthMinuteHand =
-                getFloat(R.styleable.AnalogClockView_strokeWidthMinuteHand, 0.0f)
-            val strokeWidthSecondHand =
-                getFloat(R.styleable.AnalogClockView_strokeWidthSecondHand, 0.0F)
+            val hourHandStrokeWidth =
+                getFloat(R.styleable.AnalogClockView_strokeWidthHourHand, DEFAULT_STROKE_WIDTH)
+            val minuteHandStrokeWidth =
+                getFloat(R.styleable.AnalogClockView_strokeWidthMinuteHand, DEFAULT_STROKE_WIDTH)
+            val secondHandStrokeWidth =
+                getFloat(R.styleable.AnalogClockView_strokeWidthSecondHand, DEFAULT_STROKE_WIDTH)
         }
 
         typedArray.recycle()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        initParams()
+
+        // Draw
+        drawCircle(canvas)
+    }
+
+    private fun drawCircle(canvas: Canvas?) {
+        canvas?.drawCircle(centreX.toFloat(), centreY.toFloat(), radius.toFloat(), circlePaint!!)
+    }
+
+    private fun initAllPaints() {
+        initCirclePaint()
+    }
+
+    private fun initCirclePaint() {
+        circlePaint = Paint(Paint.ANTI_ALIAS_FLAG)
+        setPaintParams(circlePaint!!, Color.BLACK, CIRCLE_STROKE_WIDTH)
+    }
+
+    private fun setPaintParams(paint: Paint, color: Int, strokeWidth: Float) {
+        paint.reset()
+        paint.color = color
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = strokeWidth
     }
 
 }
